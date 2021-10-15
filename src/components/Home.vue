961ff6bb9eadbe4a1d226c014752a1846c7c131c
Antoine Le Bras <alebras@expert-net.fr>
2021-10-15 16:17:09 +0200

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main-container">
+    <!-- Landing -->
+    <div class="section landing" id="landing">
+      <!-- Banner -->
+      <div class="banner">
+        <img src="../assets/lbAntoine@2x.png" class="logo">
+        <div class="nav-bar">
+          <a href="#landing" class="active">Home</a>
+          <a href="#about">About</a>
+          <a href="">Work</a>
+          <a href="">Resume</a>
+          <button>Contact Antoine</button>
+        </div>
+      </div>
+    </div>
+
+    <!-- About -->
+    <div class="section about" id="about">
+      <div class="text-wrapper">
+        <h1 class="about-title">I am a <span>web developer</span><br>and a <span>tech enthousiast</span></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <img class="about-picture" src="../assets/rec.jpg">
+    </div>
+
+    <!-- About 2 -->
+    <div class="section about-2" id="about-2">
+      <img class="about-picture-2" src="../assets/childme.jpg">
+      <div class="text-wrapper">
+          <h1>I studied <span>arts</span> and <span>cinema</span></h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+    </div>
+
+    <!-- PARTY-3 -->
+    <div class="section party" id="partylax">
+      <div class="layer"></div>
+    </div>
+
+    <!-- End -->
+    <div class="section end" id="around">
+      <h1 class="mot-de-fin">I am also an <span>amateur photographer</span><br>and <span>philosophy reader</span>. A <span>cat lover</span>,<br><span>card game player</span>, <span>coffee drinker</span> and <span>pizza fanatic</span>.</h1>
+      <p class="footer-text">fueled with fun and big dreams :)</p>
+      <div class="footer-socials"></div>
+    </div>
+
+  </div>
+</template>
+
+<style lang="css">
+  @import '../styles/processed/Home.css';
+</style>
